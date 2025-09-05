100d6267e8b5931105b565663ed8a362d8d4715e
jseobyun <jseob.y@polygom.xyz>
2025-09-05 13:40:12 +0900

    k_hairstyle script

--- scripts/reconstruct.py
+++ scripts/reconstruct.py
@@ -11,20 +11,25 @@ if __name__ == "__main__":
 
     recon = Reconstructor(
         calibration_level=0,  # [High 0 1 2 3 4 Low], image resolution control
-        mesh_level=0,  # [High 0 1 2 3 4 Low], MVS reoslution control
+        mesh_level=1,  # [High 0 1 2 3 4 Low], MVS reoslution control
         texture_size=4096,
-        bbox_dim=[5, 5, 5],  # width height depth, height is updirection
+        bbox_dim=[2, 2, 2],  # width height depth, height is updirection
     )
 
-    img_dir = "/media/jseob/SSD_HEAD/ava256/20210817--0900--NRE683/decoder/image_jpg/043131"
-    save_dir = "/media/jseob/SSD_HEAD/ava256/20210817--0900--NRE683/decoder/results"
-    init_dir = "/media/jseob/SSD_HEAD/ava256/20210817--0900--NRE683/decoder"
+    root = "/media/jseob/3D-PHOTO-03/k_hairstyle_raw/Training/masked"
+    style_names = sorted(os.listdir(root))
+    for style_name in style_names:
+        subj_names = sorted(os.listdir(os.path.join(root, style_name)))
+        for subj_name in subj_names:
 
-    img_names = sorted(os.listdir(img_dir))
-    img_paths = [os.path.join(img_dir, img_name) for img_name in img_names]
+            img_dir = os.path.join(root, style_name, subj_name)
+            save_dir = img_dir.replace("masked", "results")            
 
-    # recon.run(img_paths, save_dir, init_dir="/media/jseob/SSD_HEAD/renderme360/processed/0039/e0/")
-    recon.run(img_paths, save_dir, init_dir=init_dir, share_intrinsic=False, format="ava256", vis=True)
+            img_names = sorted(os.listdir(img_dir))
+            img_paths = [os.path.join(img_dir, img_name) for img_name in img_names]
+
+            # recon.run(img_paths, save_dir, init_dir="/media/jseob/SSD_HEAD/renderme360/processed/0039/e0/")
+            recon.run(img_paths, save_dir, share_intrinsic=False)
 
     
 
